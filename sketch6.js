54f1c1ef3966ac1699e1a8c67aa7b2bfb143e8cc
jipazmino <jpazminomino@cca.edu>
2017-02-22 11:25:59 -0800

    Create sketch6.js

---
+++ sketch6.js
@@ -0,0 +1,19 @@
+function setup() { 
+  createCanvas(400, 400);
+} 
+
+function draw() { 
+  background(220);
+
+  
+  for (var y = 10; y <= 390; y = y + 10) {
+    if (y<=200){
+    for (var x = y; x<=400-y ; x = x + 10) {
+      ellipse (x,y,10,10);
+      }
+  } else {
+    for (var x = 400-y; x<=y; x = x +10) {
+      ellipse (x,y,10,10)}
+  }
+  }
+}
