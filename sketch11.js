96ce11777b2cd33015c6e1369b2b3ceadbc3d47f
jipazmino <jpazminomino@cca.edu>
2017-02-22 11:36:54 -0800

    Create sketch11.js

---
+++ sketch11.js
@@ -0,0 +1,20 @@
+function setup() { 
+  createCanvas(400, 400);
+  frameRate(8);
+} 
+
+function draw() { 
+  background(220);
+
+
+
+  
+  
+for (x=0;x<=400;x=x+20){
+    strokeWeight(6);
+  line (x,400,x,random(0,370));
+    
+    
+}}
+
+
